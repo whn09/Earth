5076126f121afa861ae36932b6c1f244442c7efb
Ubuntu <ubuntu@ip-192-168-6-51.ec2.internal>
2018-07-24 07:57:23 +0000

    add tensorboard, add prediction

--- candidates_generator.py
+++ candidates_generator.py
@@ -3,16 +3,22 @@ import tensorflow as tf
 import time
 import math
 import helper
+import os
 
 #train_file = "data/dbpedia.train"
 #test_file = "data/dbpedia.test"
 train_file = "../dataset/haha_new/haha_new.raw.20180712.train"
 test_file = "../dataset/haha_new/haha_new.raw.20180712.test"
 
-max_window_size = 1000
-batch_size = 500
+# Number of Epochs
+num_epochs = 10
+max_window_size = 100
+batch_size = 300
+learning_rate_value = 0.01
+show_every_n_batches = 10
+log_dir = 'logs/'
 
-label_to_int,vocab_to_int = helper.build_vocab_dict(train_file)
+label_to_int,vocab_to_int,int_to_label,int_to_vocab = helper.build_vocab_dict(train_file)
 print ('label_to_int:', len(label_to_int))
 print ('label_to_int:', label_to_int)
 print ('vocab_to_int:', len(vocab_to_int))
@@ -27,24 +33,30 @@ def get_batches(start_pos, batch_size, train_data):
     y = []
     word_num = np.zeros((batch_size))
     line_no = 0
+    col_no_stat = {}
     for line in batch:
         line = line.strip().lower().split(' ')
         col_no = 0
+        for i in line:
+            if i in label_to_int:
+                y.append(label_to_int[i])
         for i in line:
             if i in vocab_to_int:
                 x[line_no][col_no] = vocab_to_int[i]
                 mask[line_no][col_no] = 1
                 col_no += 1
-            elif i in label_to_int:
-                y.append(label_to_int[i])
             if col_no >= max_window_size:
                 break
         word_num[line_no] = col_no
         line_no += 1
+        if col_no not in col_no_stat:
+            col_no_stat[col_no] = 0
+        col_no_stat[col_no] += 1
 
     y = np.array(y).reshape(batch_size, 1)
     batch_mask = mask.reshape(batch_size, max_window_size, 1)
     word_number = word_num.reshape(batch_size, 1)
+    #print('col_no_stat:', col_no_stat)
     return x,y,batch_mask,word_number
 
 def get_inputs():
@@ -98,33 +110,50 @@ with train_graph.as_default():
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
     out_layer = tf.matmul(pred, tf.transpose(nce_weights)) + nce_biases
 
-# Number of Epochs
-num_epochs = 100
-batch_size = 300
-learning_rate_value = 0.01
-show_every_n_batches = 10
-
 with tf.Session(graph=train_graph) as sess:
-    sess.run(tf.global_variables_initializer())
+    #Initialize Saver
+    saver=tf.train.Saver()
+    # Open a writer to write summaries.
+    tf.summary.scalar('loss', loss)
+    tf.summary.scalar('cost', cost)
+    merged = tf.summary.merge_all()
+    writer = tf.summary.FileWriter(log_dir, sess.graph)
+    if os.path.exists(log_dir+"checkpoint"):
+        print("Restoring Variables from Checkpoint.")
+        saver.restore(sess,tf.train.latest_checkpoint(log_dir))
+    else:
+        print('Initializing Variables')
+        sess.run(tf.global_variables_initializer())
 
     # Training cycle
     start_time = time.time()
     total_batch = int(len(vocab_to_int) / batch_size)
     print("total_batch of training data: ", total_batch)
-    for epoch in range(num_epochs):
+    save_path=log_dir+"model.ckpt"
+    for epoch in range(num_epochs):  # TODO should incre epoch if use saver.restore
         avg_cost = 0.
         for i in range(total_batch):
             x, y, batch_mask, word_number = get_batches(i * batch_size, batch_size, train_data)
-            _,c = sess.run([optimizer, cost], feed_dict={inputs: x, targets: y,
+            summary,_,c = sess.run([merged, optimizer, cost], feed_dict={inputs: x, targets: y,
                                                          emb_mask: batch_mask, vocab_num: word_number,
                                                          learning_rate:learning_rate_value})
             avg_cost += c / total_batch
+            writer.add_summary(summary, epoch)
         # Display logs per epoch step
         if epoch % show_every_n_batches == 0:
+            end_time = time.time()
             print("Epoch:", '%04d' % (epoch + 1), "cost=", \
-              "{:.9f}".format(avg_cost))
+              "{:.9f}".format(avg_cost), "time=", "{:.4f}".format(end_time-start_time))
+            start_time = time.time()
+            saver.save(sess,save_path,global_step=epoch)
 
+    saver.save(sess,save_path,global_step=epoch)
+    writer.close()
+    
+    fout = open(log_dir+'preds.txt', 'w')
+    fout.write('title+body\ttrue\tpred\n')
     # Test model
+    prediction = tf.argmax(out_layer, 1)
     correct_prediction = tf.equal(tf.argmax(out_layer, 1), tf.reshape(targets, [batch_size]))
     # Calculate accuracy
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
@@ -136,7 +165,18 @@ with tf.Session(graph=train_graph) as sess:
         batch_accuracy = accuracy.eval({inputs: x, targets: y,
                                         emb_mask: batch_mask, vocab_num: word_number,
                                         learning_rate: learning_rate_value})
+        preds = prediction.eval({inputs: x, targets: y,
+                                 emb_mask: batch_mask, vocab_num: word_number,
+                                 learning_rate: learning_rate_value})
+        #print('preds:', preds)
+        for j in range(len(x)):
+            xj = []
+            for k in range(len(x[j])):
+                if int(x[j][k]) > 0:
+                    xj.append(int_to_vocab[int(x[j][k])])
+            fout.write(' '.join(xj)+'\t'+int_to_label[int(y[j])]+'\t'+int_to_label[preds[j]]+'\n')
         print("Batch Accuracy: ", batch_accuracy)
         final_accuracy += batch_accuracy
     print("Final Accuracy: ", final_accuracy * 1.0 / total_batch)
+    fout.close()
 
--- helper.py
+++ helper.py
@@ -8,6 +8,8 @@ def build_vocab_dict(path):
     print('linecache.checkcache():', linecache.checkcache(path))
     label_to_int = {}
     vocab_to_int = {}
+    int_to_label = {}
+    int_to_vocab = {}
     count = 0
     label_cnt = 0
     vocab_cnt = 1
@@ -22,13 +24,15 @@ def build_vocab_dict(path):
             if i.find('__label__') == 0:
                 if i not in label_to_int:
                     label_to_int[i] = label_cnt
+                    int_to_label[label_cnt] = i
                     label_cnt += 1
             else:
                 if i not in vocab_to_int:
                     vocab_to_int[i] = vocab_cnt
+                    int_to_vocab[vocab_cnt] = i
                     vocab_cnt += 1
 
-    return label_to_int,vocab_to_int
+    return label_to_int,vocab_to_int,int_to_label,int_to_vocab
 
 def read_data_into_cache(path):
     return linecache.getlines(path)
